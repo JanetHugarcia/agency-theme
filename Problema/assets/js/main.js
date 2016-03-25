bac9f1fcf7162aff05f9628c3dfa4cae72873c46
JanetHugarcia <nayavz@gmail.com>
2016-03-25 12:23:27 -0500

    agregando easing

--- Problema/assets/js/main.js
+++ Problema/assets/js/main.js
@@ -2,13 +2,3 @@
 $(window).resize(function() {
         $('#hero').height($(window).height());
 }).resize();
-/*sticky*/
-var altura = $('header').offset().top;
-
-$(window).on('scroll', function(){
-	if ( $(window).scrollTop() > altura ){
-		$('header').addClass('menu-fixed');
-	} else {
-		$('header').removeClass('menu-fixed');
-	}
-});
